1a6e7a00a46adc666edba646a4790ef8170375ca
junggyeol444 <53264511+junggyeol4444@users.noreply.github.com>
2025-11-28 22:44:16 +0900

    Delete main directory

--- main/java/org/blog/minecraftJobPlugin/util/ActionBarUtil.java
+++
@@ -1,9 +0,0 @@
-package com.yourname.jobplugin.util;
-
-import org.bukkit.entity.Player;
-
-public class ActionBarUtil {
-    public static void send(Player player, String msg) {
-        player.sendActionBar(msg);  // Paper 1.21 이상 표준 API
-    }
-}
